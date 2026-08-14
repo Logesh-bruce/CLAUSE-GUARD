05dfdb4d6ef5cf4840e62324b11fc7667e04412b
Logesh-bruce <logeshmg432006@gmail.com>
2026-08-14 09:57:31 +0530

    feat: add request logging middleware

--- src/server.js
+++ src/server.js
@@ -13,6 +13,15 @@ app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.static(path.join(__dirname, '../public')));
 
+// Minimal request logger
+app.use((req, res, next) => {
+  const start = Date.now();
+  res.on('finish', () => {
+    console.log(`${req.method} ${req.originalUrl} ${res.statusCode} ${Date.now() - start}ms`);
+  });
+  next();
+});
+
 // Routes
 app.use('/api/analyze', analyzeRouter);
 app.use('/api/export', exportRouter);
